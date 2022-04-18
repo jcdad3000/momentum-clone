a752bedd781dc36e1e70f4e63efc5d44722abb79
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-04-18 09:58:59 +0900

    lec 4.2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
 
-function handleLoginBtnClick(){
-    const username = loginInput.value;
-    console.log(username)
+
+function onLoginSubmit(event){
+
+    event.preventDefault();
+    console.log(loginInput.value)
 }
-
-loginButton.addEventListener("click",handleLoginBtnClick)
+loginForm.addEventListener("submit", onLoginSubmit)
 
