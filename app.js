9da211b11fc5a1bab1c637e5e2d4c6c7dbf69ddc
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-04-18 10:52:49 +0900

    lec 4.6

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const loginForm = document.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting")
+const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();    
     const username=  loginInput.value;
-    localStorage.setItem("username", username)
+    localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username)
+}
+
+function paintGreetings(username){
     greeting.innerText =`Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-
-loginForm.addEventListener("submit", onLoginSubmit)
+if (savedUsername ===null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(savedUsername)
+}
