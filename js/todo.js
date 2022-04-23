fd287bb4e1503f80dcd65fa57e3041c072719949
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-04-24 00:12:08 +0900

    lec 7.4

--- js/todo.js
+++ js/todo.js
@@ -1,28 +1,45 @@
-const toDoForm = document.getElementById("todo-form")
-const toDoInput = toDoForm.querySelector("input")
-const toDoList = document.getElementById("todo-list")
+const toDoForm = document.getElementById('todo-form')
+const toDoInput = toDoForm.querySelector('input')
+const toDoList = document.getElementById('todo-list')
 
-function deleteTodo(event){
-    const li=event.target.parentElement;
-    li.remove();
-}
-function paintToDo(newTodo){
-    const li = document.createElement("li")
-    const span = document.createElement("span")
-    span.innerText=newTodo;
-    const btn = document.createElement("button")
-    btn.innerText = "X"
-    btn.addEventListener("click",deleteTodo)
-    li.appendChild(span)
-    li.appendChild(btn)    
-    toDoList.appendChild(li)
+const toDos = []
+
+const TODOS_KEY = 'todos'
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
 }
 
-function handleToDoSubmit(event){
-    event.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value = ""
-    paintToDo(newTodo);
+function deleteTodo(event) {
+  const li = event.target.parentElement
+  li.remove()
+}
+function paintToDo(newTodo) {
+  const li = document.createElement('li')
+  const span = document.createElement('span')
+  span.innerText = newTodo
+  const btn = document.createElement('button')
+  btn.innerText = 'X'
+  btn.addEventListener('click', deleteTodo)
+  li.appendChild(span)
+  li.appendChild(btn)
+  toDoList.appendChild(li)
 }
 
-toDoForm.addEventListener("submit",handleToDoSubmit);
+function handleToDoSubmit(event) {
+  event.preventDefault()
+  const newTodo = toDoInput.value
+  toDoInput.value = ''
+  toDos.push(newTodo)
+  paintToDo(newTodo)
+  saveToDos()
+}
+
+toDoForm.addEventListener('submit', handleToDoSubmit)
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos)
+  parsedToDos.array.forEach
+}
